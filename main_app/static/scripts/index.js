647e9c48cf18a5abb0c8ddd8e9e1fac624611bee
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-06-09 11:50:43 -0400

    signup prompt added to login html, popups back on map

--- main_app/static/scripts/index.js
+++ main_app/static/scripts/index.js
@@ -25,7 +25,7 @@ map.boxZoom.enable();
 
 for (let i = 0; i < $lat.length; i++){
 
-  const popup = new mapboxgl.Popup({ offset: 25 }).setHTML($name[i].innerText, $img[i].src);
+  const popup = new mapboxgl.Popup({ offset: 25 }).setHTML($name[i].innerText);
   const el = document.createElement('div');
   el.id = 'marker';
 
@@ -35,4 +35,3 @@ for (let i = 0; i < $lat.length; i++){
   .addTo(map);
 
   }
-
