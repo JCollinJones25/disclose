2ee99310314e12d8ac94164425ea096ce8a9d316
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-06-10 15:31:49 -0400

    secret key

--- capstone/settings.py
+++ capstone/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-3$7yoxygx!)e&(t1l#&$kx32^qp5ky2m1iwclz)d-7v81z(-e('
 
 API_KEY = os.getenv("API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
