3baa89b64bebd5f4001a3805a2274a63028b6e0a
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-06-10 10:00:56 -0400

    Config deployment

--- capstone/settings.py
+++ capstone/settings.py
@@ -137,3 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
